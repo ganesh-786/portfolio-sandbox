346619264614d3e4eb8aee38187c15ab67fa7c03
ganesh-786 <trumpnp72@gmail.com>
2026-09-19 20:54:11 +0545

    Rehearsal: production values restored

--- app/robots.ts
+++ app/robots.ts
@@ -3,5 +3,11 @@ import type { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: '*', disallow: '/' } }
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://ganeshtharu.com.np/sitemap.xml',
+  }
 }
